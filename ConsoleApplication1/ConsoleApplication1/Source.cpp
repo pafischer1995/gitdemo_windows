018bc60d13a09235280497832060ecb1039fc608
Paul A. Fischer <pfischer@carthage.edu>
2017-04-20 15:17:06 -0500

    first addition of code in Visual Studio

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,14 @@
+//Paul A. Fischer
+//Computer Science II
+//Perry Kivolowitz
+//Github Project 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	return 0;
+}
